b5ef697cda860968e1aa4cfacd00570b04cd9ba4
its572 <zahraahamadkhalaf@gmail.com>
2020-09-26 19:46:33 +0300

    done

---
+++ Intel/GfxCPLBatchFiles/{EC94D02F-D200-4428-9531-05AF7F9799CB}.bat
@@ -0,0 +1,4 @@
+@echo off
+TIMEOUT 1 /NOBREAK
+if exist "C:\WINDOWS\System32\DriverStore\FileRepository\cui_dch.inf_amd64_1cc7b402747a872f\igfxEMN.exe" start "" "C:\WINDOWS\System32\DriverStore\FileRepository\cui_dch.inf_amd64_1cc7b402747a872f\igfxEMN.exe"
+del /Q "C:\Intel\GfxCPLBatchFiles\{EC94D02F-D200-4428-9531-05AF7F9799CB}.bat"
---
+++ Intel/app/src/main/java/com/example/day/Movie.java
@@ -0,0 +1,9 @@
+package com.example.day;
+
+public class Movie {
+    String title = "";
+    String mainActor = "";
+    double movieRate = 2.5;
+    int pgRate = 1 ;
+    String Genre = "";
+}
